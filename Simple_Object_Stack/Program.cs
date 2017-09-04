224b22a0a66bbc81fda06259a36a92cfee29651f
mseneque <mattseneque@gmail.com>
2017-09-04 14:47:56 +0800

    Initial Commit

---
+++ Simple_Object_Stack/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Simple_Object_Stack
+{
+    // A Stack is a data structure for storing a list of elements in a LIFO (last in, first out) fashion.
+    // This simple object stack has three methods; Push(), Pop() and Clear()
+    // This stack uses the object class, which has the downside of boxing value types to object reference types 
+    // when Pushed to the stack, and then unboxing them back to the original value type when Popped back of the stack.
+    // Boxing and unboxing comes with a small performance penalty. However, another implementation using generics 
+    // will resolve this issue, but hasn't been implemented in this stack class.
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            stack.Push(1);
+            stack.Clear();
+
+            stack.Push(2);
+            stack.Push(DateTime.Today);
+            stack.Push("Hello");
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+            stack.Clear();
+            stack.Clear();
+
+        }
+    }
+}
---
+++ Simple_Object_Stack/Stack.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+
+namespace Simple_Object_Stack
+{
+    partial class Program
+    {
+        public class Stack
+        {
+            private readonly ArrayList _stackList;
+
+            public Stack()
+            {
+                _stackList = new ArrayList();
+            }
+
+            public void Push(object obj)
+            {
+                if (obj == null)
+                    throw new Exception("InvalidOperationException");
+                _stackList.Add(obj);
+            }
+
+            public object Pop()
+            {
+                var popIndex = _stackList.Count - 1;
+
+                if (popIndex >= 0)
+                {
+                    var obj = _stackList[popIndex];
+                    _stackList.RemoveAt(popIndex);
+                    return obj;
+                }
+
+                throw new Exception("InvalidOperationException");
+
+            }
+
+            public void Clear()
+            {
+                _stackList.Clear();
+            }
+        }
+    }
+}
